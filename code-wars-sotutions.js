f65a4eb0ca1a4dd1a15a4c82923a8a35b639edff
apatterson0208 <apatterson0208@gmail.com>
2023-01-04 21:51:50 +0000

    Completed questions 1-4 with explanations

--- code-wars-sotutions.js
+++ code-wars-sotutions.js
@@ -1,1 +1,77 @@
-// Copy/Paste you solutions below. Each solution should be accompanied by a multiline comment explaining the solution.
+/* In the first line, I am initializing a variable to hold the sum of all multiples.
+   In the second line, I created a for loop to loop through all numbers from one to the number given
+   In the third line, I am setting the condition to check if the iterator is a multiple of 5 or 3.
+   If the condition is met, the number is added to the variable multipleSum and when the loop is finished, multipleSum is returned;
+*/
+function solution(number){
+  let multipleSum = 0;
+  for (let i = 1; i < number; i++){
+    if (i % 5 === 0 || i % 3 === 0){
+      multipleSum += i;
+    }
+  }
+  return multipleSum;
+}
+
+/*
+    In this code, I set several conditionals to check for the length of the names array. If the length of the array was less than four, each name in the array was included in the "... like(s) this" string. If the length of the array was greater than 4, the first two names are included in the array, and then the length of the array minus the first two names was added into the string as ".. others."
+*/
+function likes(names) {
+    if (names.length === 0){
+      return "no one likes this";
+    } else if (names.length === 1){
+      return `${names[0]} likes this`
+    } else if (names.length === 2){
+      return `${names[0]} and ${names[1]} like this`
+    } else if (names.length === 3){
+      return `${names[0]}, ${names[1]} and ${names[2]} like this`
+    } else if (names.length >= 4){
+      return `${names[0]}, ${names[1]} and ${names.length-2} others like this`
+    }
+}
+
+/*
+    In the first line, I'm initializing an object. Then, I create a loop iterate through the array and see if the element exists as a key in the object. If it does, increment the value by one. If not, create the key and set the value to one. Then, I check to see which key in the object has a value of one, and I return that value.
+*/
+function findUniq(arr) {
+  const numberObj = {};
+  
+  for (let nums of arr){
+    if (nums in numberObj){
+      numberObj[nums]++;
+    } else {
+      numberObj[nums] = 1;
+    }
+  }
+  for (let key in numberObj){
+    if (numberObj[key] === 1){
+      return Number(key);
+    }
+  }
+}
+
+/*
+    In the first two lines, I created an array to hold the odd integers and one to hold the even numbers. I then looped through integers array and pushed the even numbers into the even array and the odd numbers into the odd array. Finally, I returned the element of whichever array held only one number. 
+
+*/
+function findOutlier(integers){
+  const evenArr = [];
+  const oddArr = [];
+  
+  for (let num of integers){
+    if(num % 2 === 0){
+      evenArr.push(num);
+    } else if (num % 2 !== 0){
+      oddArr.push(num);
+    }
+  }
+  if (evenArr.length === 1){
+    return evenArr[0];
+  } else if (oddArr.length === 1){
+    return oddArr[0];
+  }
+}
+
+/*
+
+*/
